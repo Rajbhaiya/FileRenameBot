9486d6324cc7d16260b053da4906c4f65536b0c2
Rajbhaiya <103052286+Rajbhaiya@users.noreply.github.com>
2023-08-03 20:21:55 +0530

    Update video_converter.py

--- plugins/video_converter.py
+++ plugins/video_converter.py
@@ -85,7 +85,7 @@ async def convert_to_video(bot, update):
            # await bot.edit_message_text(
               #  text=Translation.UPLOAD_START,
              #   chat_id=update.chat.id,
-            #    message_id=a.id
+            #    message_id=a.message_id
           #  )
             logger.info(the_real_download_location)
             # get the correct width, height, and duration for videos greater than 10MB
