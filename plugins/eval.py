33d6bffc61ff4eb3e391b01b130c2382a2f5b519
136574884+Yi-Zaha@users.noreply.github.com <136574884+Yi-Zaha@users.noreply.github.com>
2023-08-05 23:16:24 +0530

    Update eval.py

--- plugins/eval.py
+++ plugins/eval.py
@@ -1,79 +1,65 @@
 import io
-import sys
 import traceback
+import sys
 
-from pyrogram import filters 
+from pyrogram import filters
 from pyrogram import Client as Mai_bOTs
-from pyrogram.enums import ParseMode
 
 ALLOWED_USERS = [5370531116, 5551387300, 5905126281, 5905126281, 5591954930]
 MAX_MESSAGE_LENGTH = 4096
 
-@Mai_bOTs.on_message(filters = filters.command("eval") &
-                filters.user(ALLOWED_USERS))
-async def _(client, message):
-    status_message=await message.reply_text("Processing ...")
+@Mai_bOTs.on_message(filters.command("eval") & filters.user(ALLOWED_USERS))
+async def evaluate_command(client, message):
+    status_message = await message.reply_text("Processing ...")
     try:
-        cmd=message.text.markdown.split(" ", maxsplit = 1)[1]
-    except BaseException:
+        cmd = message.text.markdown.split(" ", maxsplit=1)[1]
+    except IndexError:
         return await status_message.edit_text("Give code to evaluate...")
 
-    reply_to_=message
-    if message.reply_to_message:
-        reply_to_=message.reply_to_message
+    reply_to_msg = message.reply_to_message or message
+    stdout, stderr, exc = None, None, None
 
-    old_stderr=sys.stderr
-    old_stdout=sys.stdout
-    redirected_output=sys.stdout=io.StringIO()
-    redirected_error=sys.stderr=io.StringIO()
-    stdout, stderr, exc=None, None, None
+    # Capture and redirect stdout and stderr
+    old_stdout, old_stderr = sys.stdout, sys.stderr
+    sys.stdout = redirected_output = io.StringIO()
+    sys.stderr = redirected_error = io.StringIO()
 
     try:
-        await aexec(cmd, client, message)
+        await execute_code(cmd, client, message)
     except Exception:
-        exc=traceback.format_exc()
+        exc = traceback.format_exc()
 
-    stdout=redirected_output.getvalue()
-    stderr=redirected_error.getvalue()
-    sys.stdout=old_stdout
-    sys.stderr=old_stderr
+    stdout = redirected_output.getvalue()
+    stderr = redirected_error.getvalue()
+    sys.stdout = old_stdout
+    sys.stderr = old_stderr
 
-    evaluation=""
-    if exc:
-        evaluation=exc
-    elif stderr:
-        evaluation=stderr
-    elif stdout:
-        evaluation=stdout
-    else:
-        evaluation="Success"
+    evaluation = exc or stderr or stdout or "Success"
 
-    final_output="**EVAL**: "
-    final_output += f"`{cmd}`\n\n"
-    final_output += "**OUTPUT**:\n"
-    final_output += f"`{evaluation.strip()}`\n"
+    final_output = f"**EVAL**: `{cmd}`\n\n**OUTPUT**:\n`{evaluation.strip()}`"
 
     if len(final_output) > MAX_MESSAGE_LENGTH:
         with io.BytesIO(str.encode(evaluation)) as out_file:
-            out_file.name="eval.text"
-            await reply_to_.reply_document(
-                document = out_file,
-                caption = f"`{cmd[: MAX_MESSAGE_LENGTH // 4 - 1]}`",
-                disable_notification = True,
-                parse_mode = ParseMode.MARKDOWN,
-                quote = True,
+            out_file.name = "eval.txt"
+            await reply_to_msg.reply_document(
+                document=out_file,
+                caption=f"`{cmd[:MAX_MESSAGE_LENGTH // 4 - 1]}`",
+                disable_notification=True,
+                parse_mode="markdown",
+                quote=True,
             )
     else:
-        await reply_to_.reply_text(final_output, parse_mode=ParseMode.MARKDOWN, quote=True)
+        await reply_to_msg.reply_text(final_output, parse_mode=ParseMode.MARKDOWN, quote=True)
     await status_message.delete()
 
-
-async def aexec(code, client, message):
+async def execute_code(code, client, message):
+    # Prepare the code to be executed with necessary context
     exec(
         "async def __aexec(client, message): "
-        + "\n m = message"
-        + "\n chat = m.chat.id"
-        + "\n reply = m.reply_to_message"
-        + "".join(f"\n {l_}" for l_ in code.split("\n"))
+        + "\n    m = message"
+        + "\n    chat = m.chat.id"
+        + "\n    reply = m.reply_to_message"
+        + "".join(f"\n    {line}" for line in code.split("\n"))
     )
+    # Execute the prepared code within the context
     return await locals()["__aexec"](client, message)
